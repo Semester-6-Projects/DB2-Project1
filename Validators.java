0f6fbc6c65572eb8993110a3374c41517c045cc4
shamy18 <youssef.elshamy8@gmail.com>
2024-04-14 17:59:57 +0200

    Validators updated

--- Validators.java
+++ Validators.java
@@ -2,6 +2,11 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Hashtable;
+import java.util.Set;
+import java.util.Arrays;
+import java.util.List;
+
 
 public class Validators {
     public static boolean checkTableExistsMF(String tableName) throws DBAppException {
@@ -56,5 +61,142 @@ public class Validators {
             throw new RuntimeException(e);
         }
         return false;
+    }
+
+
+    private void generalExcCheck(String tablename, String key, String keyType) throws DBAppException {
+        Boolean keyattr = false;
+        Boolean table = false;
+        int i = 0;
+        BufferedReader br;
+        try {
+            br = new BufferedReader(new FileReader("resources/metaFile.csv"));
+            String ln = br.readLine();
+
+            while (ln != null) {
+                String[] a = ln.split(",");
+                if (a[0].equals(tablename)) {
+                    table = true;
+                    i++;
+                    if (a[1].equals(key)) {
+                        keyattr = true;
+                        if (!(keyType.equals(a[2]))) {
+                            DBAppException exc = new DBAppException(key + " should be of type: "
+                                    + a[2]);
+                            throw exc;
+                        }
+                    }
+
+                }
+
+                ln = br.readLine();
+
+            }
+
+            br.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (!(table)) {
+            DBAppException exc = new DBAppException(
+                    "Table can't be found");
+            throw exc;
+        }
+        if (!(keyattr)) {
+            DBAppException exc = new DBAppException(
+                    "There's no attribute in table" + tablename + " with the name" + key);
+            throw exc;
+        }
+
+    }
+
+    private void allowedKeyType(Hashtable<String, String> htblColNameType) throws DBAppException {
+        Set<String> keys = htblColNameType.keySet();
+        List<String> datatypes = Arrays.asList("java.lang.Integer", "java.lang.Double", "java.lang.String");
+        int i = 0;
+        for (String key : keys) {
+            if (!(datatypes.contains((htblColNameType.get(key)))))
+                throw new DBAppException("The type of the key is invalid. It has be either Integer or Double or String");
+        }
+    }
+    private void allowedOps(String op) throws DBAppException {
+
+        List<String> operators = Arrays.asList(">",">=","<","<=","!=","=");
+
+        if (!(operators.contains(op)))
+            throw new DBAppException(
+                    "The operator has to be one of the following: >, >=, <, <=, != , =");
+
+    }
+
+    private void allowedSTROps(String strOp) throws DBAppException {
+
+        List<String> operators = Arrays.asList("AND", "OR", "XOR");
+
+        if (!(operators.contains(strOp.toUpperCase())))
+            throw new DBAppException(
+                    "The operator has to be one of the following: AND, OR, XOR");
+
+    }
+
+    private int numOfCols(String tablename) throws DBAppException {
+        int i = 0;
+        BufferedReader br;
+        try {
+            br = new BufferedReader(new FileReader("resources/metaFile.csv"));
+            String ln = br.readLine();
+            while (ln != null) {
+                String[] a = ln.split(",");
+                if (a[0].equals(tablename))
+                    i++;
+
+                ln = br.readLine();
+            }
+
+            br.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return i;
+
+    }
+
+
+    private void checkTableName(String tablename) throws DBAppException {
+        Boolean flag = false;
+        BufferedReader br;
+        try {
+            br = new BufferedReader(new FileReader("resources/metaFile.csv"));
+            String ln = br.readLine();
+            while (ln != null) {
+                String[] a = ln.split(",");
+                if (a[0].equals(tablename))
+                    flag = true;
+
+                ln = br.readLine();
+
+            }
+            br.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (flag) {
+            DBAppException exc = new DBAppException(
+                    "Table already exists");
+            throw exc;
+        }
+
     }
 }
